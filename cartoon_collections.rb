8fd6cca712b21865f4345319b99d0570230d4fb5
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-06 23:09:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def long_planeteer_calls(array)
 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
